a0fd76ccb45ce03675a4b1f96d85b67df1442f5e
mineejo <mineejo.dev@gmail.com>
2023-11-11 15:56:10 +0300

    chore(mod.ts): update doc module

--- mod.ts
+++ mod.ts
@@ -1,4 +1,10 @@
 // Copyright 2023 mineejo. All rights reserved. MIT license.
 
+/**
+ * Colors is JSON with the names of colors, their values and localization. Colors are based on HTML color names and CSS keywords: https://en.wikipedia.org/wiki/Web_colors#HTML_color_names.
+ *
+ * @module
+ */
+
 export { basicColors } from "./basic_colors.ts";
 export { colors } from "./colors.ts";
